e8eaecfdf45ec416a9350099b926e7d596fbf7ba
soroushfathi <soroush8fathi@gmail.com>
2025-10-14 18:04:47 +0330

    Add argument description in the docstring

--- src/mqt/bench/benchmark_generation.py
+++ src/mqt/bench/benchmark_generation.py
@@ -93,7 +93,7 @@ def _create_mirror_circuit(
     Args:
         qc_original: The quantum circuit to mirror.
         inplace: If True, modifies the circuit in place. Otherwise, returns a new circuit.
-        basis_gates: List of native gates of the target device.
+        target: List of native gates of the target device.
         optimization_level: Optimization level of the device.
 
     Returns:

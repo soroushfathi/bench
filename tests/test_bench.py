e4cb4253239cd0b9e1097058c41ecbd39a66f151
soroushfathi <soroush8fathi@gmail.com>
2025-10-14 07:31:07 +0330

    Fix import

--- tests/test_bench.py
+++ tests/test_bench.py
@@ -20,6 +20,7 @@ from importlib import metadata
 from pathlib import Path
 from typing import TYPE_CHECKING, NoReturn, cast
 
+import numpy as np
 import pytest
 from qiskit import QuantumCircuit, qpy
 from qiskit.circuit import Parameter
